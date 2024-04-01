7e669fee8d7c4db4cc50b6f59ad7c18993f91d8e
jaya2444 <106364569+jaya2444@users.noreply.github.com>
2024-04-01 18:15:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function display(a){
+    document.getElementById("input").value+=a;
+    
+    }
+    
+    function calculation(){
+    var x =document.getElementById("input").value;
+    document.getElementById("input").value=eval(x);
+    
+
+    }
+    
+    function hii(){
+        document.getElementById("input").value="";
+    
+    
+    
+    
+    }
